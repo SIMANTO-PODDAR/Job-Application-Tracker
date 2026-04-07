4f15fe8206547c0c4d2e13401e4d61c9191cece0
Simanto Poddar <251956045+SIMANTO-PODDAR@users.noreply.github.com>
2026-04-07 21:28:07 +0600

    More Responsive Version

--- script.js
+++ script.js
@@ -2,53 +2,53 @@ let headerAllJob2 = document.getElementById('h-all-job-count2');
 
 let allJob = document.getElementById('all-job');
 let headerAllJob1 = document.getElementById('h-all-job-count1');
-    headerAllJob1.innerText = allJob.children.length;             //   set all job count
-    headerAllJob2.innerText = allJob.children.length; 
+headerAllJob1.innerText = allJob.children.length;             //   set all job count
+headerAllJob2.innerText = allJob.children.length;
 
 //                 get tgl btn
-let allBtnTgl    = document.getElementById('tgl-all');
-let interBtnTgl  = document.getElementById('tgl-itrvew');
+let allBtnTgl = document.getElementById('tgl-all');
+let interBtnTgl = document.getElementById('tgl-itrvew');
 let rejectBtnTgl = document.getElementById('tgl-rjkt');
 
-allBtnTgl.addEventListener('click', function() {
+allBtnTgl.addEventListener('click', function () {
     let allPosts = document.getElementsByClassName('allPsts');
-    
-    for(let post of allPosts){
+
+    for (let post of allPosts) {
         post.classList.remove('hidden');
     }
     noDataA();
-    yFoTtal ('c',0);
+    yFoTtal('c', 0);
 })
-    
-interBtnTgl.addEventListener('click', function() {
+
+interBtnTgl.addEventListener('click', function () {
     let allPosts = document.getElementsByClassName('allPsts');
     let intPosts = document.getElementsByClassName('interview');
-    
-    for(let post of allPosts){
+
+    for (let post of allPosts) {
         post.classList.add('hidden');
     }
-    
-    for(let intPost of intPosts){
-         intPost.classList.remove('hidden');
+
+    for (let intPost of intPosts) {
+        intPost.classList.remove('hidden');
     }
 
     noDataI();
     let interviewCount = document.getElementsByClassName('interview').length;  //   cls COUNT IN NUM access
     noDataI2(interviewCount);
 
-    yFoTtal('a',interviewCount);
-    
+    yFoTtal('a', interviewCount);
+
 })
-    
-rejectBtnTgl.addEventListener('click', function() {
+
+rejectBtnTgl.addEventListener('click', function () {
     let allPosts = document.getElementsByClassName('allPsts');
     let rejPosts = document.getElementsByClassName('rejected');
-    
-    for(let post of allPosts){
+
+    for (let post of allPosts) {
         post.classList.add('hidden');
     }
-    
-    for(let rejPost of rejPosts){
+
+    for (let rejPost of rejPosts) {
         rejPost.classList.remove('hidden');
     }
 
@@ -57,47 +57,47 @@ rejectBtnTgl.addEventListener('click', function() {
     let rejectedCount = document.getElementsByClassName('rejected').length;
     noDataR2(rejectedCount);
 
-    yFoTtal('b',rejectedCount);
-    
+    yFoTtal('b', rejectedCount);
+
 })
 
 //                 onclick btn color
-function btnColor(id){
-    allBtnTgl.classList.remove('bg-blue-500','text-amber-50');
-    interBtnTgl.classList.remove('bg-blue-500','text-amber-50');
-    rejectBtnTgl.classList.remove('bg-blue-500','text-amber-50');
+function btnColor(id) {
+    allBtnTgl.classList.remove('bg-blue-500', 'text-amber-50');
+    interBtnTgl.classList.remove('bg-blue-500', 'text-amber-50');
+    rejectBtnTgl.classList.remove('bg-blue-500', 'text-amber-50');
 
-    let slctBtn= document.getElementById(id);
+    let slctBtn = document.getElementById(id);
 
-    slctBtn.classList.add('bg-blue-500','text-amber-50');
+    slctBtn.classList.add('bg-blue-500', 'text-amber-50');
 
 }
 
 // update Status 
-function updateSttsI(n,bT){     //bT btn typ
-    let normalBtn = document.getElementById('normal-'+n);
-    let greenBtn = document.getElementById('green-'+n);
-    let redBtn = document.getElementById('red-'+n);
+function updateSttsI(n, bT) {     //bT btn typ
+    let normalBtn = document.getElementById('normal-' + n);
+    let greenBtn = document.getElementById('green-' + n);
+    let redBtn = document.getElementById('red-' + n);
     normalBtn.classList.add('hidden');
     greenBtn.classList.add('hidden');
     redBtn.classList.add('hidden');
-    if(bT == 'i'){
+    if (bT == 'i') {
         greenBtn.classList.remove('hidden');
     }
-    else if(bT == 'r'){
+    else if (bT == 'r') {
         redBtn.classList.remove('hidden');
     }
 
 }
 
 // for Intervies btn
-function pstIBtnClk(id,n){
+function pstIBtnClk(id, n) {
     let btnId = document.getElementById(id);
-    btnId.parentElement.parentElement.classList.remove('rejected'); 
+    btnId.parentElement.parentElement.classList.remove('rejected');
     btnId.parentElement.parentElement.classList.add('interview');
     updateValue();
     // update Status
-    updateSttsI(n,'i');
+    updateSttsI(n, 'i');
 
     noDataI();
     let interviewCount = document.getElementsByClassName('interview').length;  //   cls COUNT IN NUM access
@@ -108,18 +108,18 @@ function pstIBtnClk(id,n){
 }
 
 // for Rejected btn
-function pstRBtnClk(id,n){
+function pstRBtnClk(id, n) {
     let btnId = document.getElementById(id);
-    btnId.parentElement.parentElement.classList.remove('interview'); 
+    btnId.parentElement.parentElement.classList.remove('interview');
     btnId.parentElement.parentElement.classList.add('rejected');
     updateValue();
     // update Status
-    updateSttsI(n,'r');
+    updateSttsI(n, 'r');
 
     noDataR();
     let rejectedCount = document.getElementsByClassName('rejected').length;
     noDataR2(rejectedCount);
-    
+
     hrdReLod();
     updateYOf();
 }
@@ -127,26 +127,26 @@ function pstRBtnClk(id,n){
 
 //   update head value
 //                              get & count All + Interview + Rejected
-function updateValue (){
-    
+function updateValue() {
+
     let allJob = document.getElementById('all-job');
     let headerAllJob1 = document.getElementById('h-all-job-count1');
     headerAllJob1.innerText = allJob.children.length;            //   set all job count
-    headerAllJob2.innerText = allJob.children.length; 
+    headerAllJob2.innerText = allJob.children.length;
 
 
     let headerInterview = document.getElementById('h-interview-job-count1');
     let headerRejected = document.getElementById('h-rejected-job-count1');
-      
+
     let interviewCount = document.getElementsByClassName('interview').length;  //   cls COUNT IN NUM access
     let rejectedCount = document.getElementsByClassName('rejected').length;
-    
+
     headerInterview.innerText = interviewCount;
     headerRejected.innerText = rejectedCount;
-    
+
     headerAllJob1.innerText = allJob.children.length;            //   set all job count
-    headerAllJob2.innerText = allJob.children.length; 
-    
+    headerAllJob2.innerText = allJob.children.length;
+
 
     noDataR2(rejectedCount);
     noDataI2(interviewCount);
@@ -154,19 +154,19 @@ function updateValue (){
 }
 
 //               delete function & updateValue
-function dltTrs(id){
+function dltTrs(id) {
     let deleteP = document.getElementById(id);
     alert(`Do you want to remove 
-        " ${deleteP.querySelector('h1').innerText } " ? `)
+        " ${deleteP.querySelector('h1').innerText} " ? `)
     deleteP.remove();
 
-    updateValue ();
+    updateValue();
     noDataI2();
-    noDataR2(); 
+    noDataR2();
     noDataI();
-    noDataR(); 
+    noDataR();
     noDataA();
-    
+
     updateYOf();
     noDataTgl();
 }
@@ -174,12 +174,12 @@ function dltTrs(id){
 //       no data
 let noData = document.getElementById('no-data');
 
-function noDataA(){
-    if(allJob.children.length == 0){
+function noDataA() {
+    if (allJob.children.length == 0) {
         noData.classList.remove('hidden')
     }
 
-    else if (allJob.children.length !== 0){
+    else if (allJob.children.length !== 0) {
         noData.classList.add('hidden')
     }
 
@@ -189,36 +189,36 @@ function noDataA(){
 let intCountInnerTxt = document.getElementById('h-interview-job-count1').innerText;
 let rejCountInnerTxt = document.getElementById('h-rejected-job-count1').innerText;
 
-function noDataI(){
-    if(intCountInnerTxt == 0 && allJob.children.length !== 0){
+function noDataI() {
+    if (intCountInnerTxt == 0 && allJob.children.length !== 0) {
         noData.classList.remove('hidden')
     }
 }
 
-function noDataR(){
-    if(rejCountInnerTxt == 0 && allJob.children.length !== 0){
+function noDataR() {
+    if (rejCountInnerTxt == 0 && allJob.children.length !== 0) {
         noData.classList.remove('hidden')
     }
 }
 
 
 // noData for rejected
-function noDataR2(N){
-    if(N == 0 && allJob.children.length !== 0){
+function noDataR2(N) {
+    if (N == 0 && allJob.children.length !== 0) {
         noData.classList.remove('hidden')
     }
 
-    else if(N>0){
+    else if (N > 0) {
         noData.classList.add('hidden')
     }
 }
 
-function noDataI2(N){
-    if(N == 0 && allJob.children.length !== 0){
+function noDataI2(N) {
+    if (N == 0 && allJob.children.length !== 0) {
         noData.classList.remove('hidden')
     }
 
-    else if(N>0){
+    else if (N > 0) {
         noData.classList.add('hidden')
     }
 }
@@ -228,21 +228,21 @@ function noDataI2(N){
 let yText = document.getElementById('y-of-total');
 let intText = document.getElementById('int-job-count-of');
 
-function yFoTtal (L,N){
+function yFoTtal(L, N) {
     yText.classList.add('hidden');
 
-    if(L == 'a'){
+    if (L == 'a') {
         yText.classList.remove('hidden');
         intText.innerText = N;
     }
-    else if(L == 'b'){
+    else if (L == 'b') {
         yText.classList.remove('hidden');
         intText.innerText = N;
     }
 
 }
 
-function updateYOf(){
+function updateYOf() {
     let intPosts = document.getElementsByClassName('interview');
     let rejPosts = document.getElementsByClassName('rejected');
 
@@ -252,12 +252,12 @@ function updateYOf(){
     let tglInAct = tglInt.classList.contains('bg-blue-500');
     let tglReAct = tglRej.classList.contains('bg-blue-500');
 
-    if(tglInAct){
-        yFoTtal ('a',intPosts.length);
+    if (tglInAct) {
+        yFoTtal('a', intPosts.length);
     }
 
-    if(tglReAct){
-        yFoTtal ('b',rejPosts.length);
+    if (tglReAct) {
+        yFoTtal('b', rejPosts.length);
     }
 }
 
@@ -277,30 +277,30 @@ function hrdReLod() {
     let alPsts = document.getElementsByClassName('allPsts');
 
 
- for (let pst of alPsts) {
-    
-    if (tglAlAct) {
-        pst.classList.remove('hidden');
+    for (let pst of alPsts) {
+
+        if (tglAlAct) {
+            pst.classList.remove('hidden');
+        }
+
+        if (tglInAct) {
+            if (pst.classList.contains('interview')) {
+                pst.classList.remove('hidden');
+            } else {
+                pst.classList.add('hidden');
+            }
+        }
+
+        if (tglReAct) {
+            if (pst.classList.contains('rejected')) {
+                pst.classList.remove('hidden');
+            } else {
+                pst.classList.add('hidden');
+            }
+        }
     }
 
-    if (tglInAct) {
-        if (pst.classList.contains('interview')) {
-            pst.classList.remove('hidden');
-        } else {
-            pst.classList.add('hidden');
-        }
-    } 
-
-    if (tglReAct) {
-        if (pst.classList.contains('rejected')) {
-            pst.classList.remove('hidden');
-        } else {
-            pst.classList.add('hidden');
-        }
-    }
-}
-
-   noDataTgl();
+    noDataTgl();
 
 }
 
